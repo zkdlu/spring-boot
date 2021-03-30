5ae9b960305462bc82c1522747c4e04c990bb305
zkdlu <zkdlu159951@gmail.com>
2021-03-30 20:33:37 +0900

    batch

---
+++ spring-batch/src/main/java/com/zkdlu/batch/job/SimpleJobConfiguration.java
@@ -0,0 +1,36 @@
+package com.zkdlu.batch.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.loader.plan.spi.JoinDefinedByMetadata;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class SimpleJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job simpleJob() {
+        return jobBuilderFactory.get("simpleJob")
+                .start(simpleStep1())
+                .build();
+    }
+
+    private Step simpleStep1() {
+        return stepBuilderFactory.get("simpleStep1")
+                .tasklet((contribution, chunckContext) -> {
+                    log.info(" >>>> This is Step1 ");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
---
+++ spring-scheduled/src/test/java/com/zkdlu/scheduled/SchedulerTest.java
@@ -0,0 +1,16 @@
+package com.zkdlu.scheduled;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SchedulerTest {
+    @Test
+    void 아아() {
+        //given
+
+        //when
+
+        //then
+    }
+}
