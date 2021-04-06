ad384226b95c3adba403dbb3cc2ecaf1509396aa
zkdlu <zkdlu159951@gmail.com>
2021-04-06 22:35:42 +0900

    api test

--- spring-tdd/src/test/java/com/zkdlu/tdd/api/OrderApiTest.java
+++ spring-tdd/src/test/java/com/zkdlu/tdd/api/OrderApiTest.java
@@ -5,6 +5,7 @@ import com.zkdlu.tdd.domain.shop.Menu;
 import com.zkdlu.tdd.domain.shop.Shop;
 import com.zkdlu.tdd.service.OrderDto;
 import com.zkdlu.tdd.service.OrderService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,19 +34,13 @@ class OrderApiTest {
     private ObjectMapper objectMapper;
 
     @Test
+    @DisplayName("주문 요청에 필요한 Parameter가 잘 매핑되었나요?")
     void placeOrder() throws Exception {
         //given
-        Shop shop = new Shop("shop-1", "건s shop", 6000);
-
-        shop.addMenu(new Menu("menu-1", "만두", 1000));
-        shop.addMenu(new Menu("menu-2", "라면", 2000));
-        shop.addMenu(new Menu("menu-3", "고기", 3000));
-        shop.open();
-
         OrderDto orderDto = new OrderDto();
         orderDto.setId("order-1");
-        orderDto.setShopId(shop.getId());
-        orderDto.setMenuIds(shop.getMenus().stream().map(Menu::getName).collect(Collectors.toList()));
+        orderDto.setShopId("shop-1");
+        orderDto.setMenuIds(Arrays.asList("menu-1","menu-2","menu-3"));
         given(orderService.placeOrder(orderDto)).willReturn("order-1");
 
         //when
