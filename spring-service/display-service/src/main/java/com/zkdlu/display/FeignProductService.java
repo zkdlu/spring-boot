c249240aa1d2e6f88c737313716937a66bb9cc07
zkdlu <zkdlu159951@gmail.com>
2021-04-17 22:49:27 +0900

    feign + hystrix

--- spring-service/display-service/src/main/java/com/zkdlu/display/FeignProductService.java
+++ spring-service/display-service/src/main/java/com/zkdlu/display/FeignProductService.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@FeignClient(name = "product", url = "http://localhost:8081/")
+@FeignClient(name = "product",
+        url = "http://localhost:8081/",
+        fallback = FeignProductServiceFallbackImpl.class)
 public interface FeignProductService {
     @RequestMapping(path = "/products")
     List<Product> getProducts();
