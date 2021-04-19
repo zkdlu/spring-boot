eaa3a1ba779102909a089cd30b6a5120a5f07a56
zkdlu <zkdlu159951@gmail.com>
2021-04-19 12:07:19 +0900

    항목 선택 slack

--- spring-slack/src/main/java/com/zkdlu/slacker/SlackApi.java
+++ spring-slack/src/main/java/com/zkdlu/slacker/SlackApi.java
@@ -1,19 +1,9 @@
 package com.zkdlu.slacker;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.slack.api.Slack;
-import com.slack.api.app_backend.interactive_components.ActionResponseSender;
-import com.slack.api.app_backend.interactive_components.payload.BlockActionPayload;
-import com.slack.api.app_backend.interactive_components.response.ActionResponse;
-import com.slack.api.util.json.GsonFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-
-import static com.slack.api.model.block.Blocks.section;
-import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
 
 @RestController
 public class SlackApi {
@@ -31,36 +21,7 @@ public class SlackApi {
     }
 
     @PostMapping(value = "/slack/callback", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public String callback(@RequestParam String payload) throws JsonProcessingException {
-        System.out.println(payload);
-
-        var blockPayload = GsonFactory.createSnakeCase()
-                .fromJson(payload, BlockActionPayload.class);
-
-        blockPayload.getMessage().getBlocks().remove(0);
-        blockPayload.getActions().forEach(action -> {
-            if (action.getActionId().equals("action_success")) {
-                blockPayload.getMessage().getBlocks().add(0,
-                        section(section -> section.text(markdownText("확인"))));
-            } else {
-                blockPayload.getMessage().getBlocks().add(0,
-                        section(section -> section.text(markdownText("취소"))));
-            }
-        });
-
-        ActionResponse response = ActionResponse.builder()
-                .replaceOriginal(true)
-                .blocks(blockPayload.getMessage().getBlocks())
-                .build();
-
-        Slack slack = Slack.getInstance();
-        ActionResponseSender sender = new ActionResponseSender(slack);
-        try {
-            sender.send(blockPayload.getResponseUrl(), response);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return "block kit";
+    public String callback(@RequestParam String payload) throws IOException {
+        return slackBot.callback(payload);
     }
 }
--- spring-slack/src/main/java/com/zkdlu/slacker/SlackBot.java
+++ spring-slack/src/main/java/com/zkdlu/slacker/SlackBot.java
@@ -1,11 +1,15 @@
 package com.zkdlu.slacker;
 
 import com.slack.api.Slack;
+import com.slack.api.app_backend.interactive_components.ActionResponseSender;
+import com.slack.api.app_backend.interactive_components.payload.BlockActionPayload;
+import com.slack.api.app_backend.interactive_components.response.ActionResponse;
 import com.slack.api.model.block.Blocks;
 import com.slack.api.model.block.LayoutBlock;
 import com.slack.api.model.block.composition.BlockCompositions;
 import com.slack.api.model.block.element.BlockElement;
 import com.slack.api.model.block.element.BlockElements;
+import com.slack.api.util.json.GsonFactory;
 import com.slack.api.webhook.WebhookPayloads;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
@@ -14,17 +18,41 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.slack.api.model.block.Blocks.section;
+import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
 import static com.slack.api.model.block.composition.BlockCompositions.plainText;
 
 @Service
 public class SlackBot {
     public String vote(String message) throws IOException {
         List<LayoutBlock> layoutBlocks = getLayoutBlocks(message);
+        layoutBlocks.add(Blocks.actions(getActionBlocks()));
 
         Slack.getInstance().send("",
                 WebhookPayloads.payload(p -> p.text("골라 골라~").blocks(layoutBlocks)));
 
         return message;
+    }
+
+    public String callback(String payload) throws IOException {
+        var blockPayload = GsonFactory.createSnakeCase()
+                .fromJson(payload, BlockActionPayload.class);
+
+        var user = blockPayload.getUser().getUsername();
+        var channel = blockPayload.getChannel().getName();
+        var actionId = blockPayload.getActions().get(0).getActionId();
+
+        var blockLayouts = getLayoutBlocks(user + "님이 " + (actionId.equals("action_success") ? "[승인]" : "[거부]" + " 하였습니다."));
+
+        ActionResponse response = ActionResponse.builder()
+                .replaceOriginal(false)
+                .blocks(blockLayouts)
+                .build();
+
+        ActionResponseSender sender = new ActionResponseSender(Slack.getInstance());
+        sender.send(blockPayload.getResponseUrl(), response);
+
+        return payload;
     }
 
     @NotNull
@@ -34,7 +62,6 @@ public class SlackBot {
 
         layoutBlocks.add(title);
         layoutBlocks.add(Blocks.divider());
-        layoutBlocks.add(Blocks.actions(getActionBlocks()));
 
         return layoutBlocks;
     }
@@ -42,17 +69,16 @@ public class SlackBot {
     @NotNull
     private List<BlockElement> getActionBlocks() {
         List<BlockElement> actions = new ArrayList<>();
-        var ok = BlockElements.button(b -> b.text(plainText("확인", true))
-                .value("ok")
-                .style("primary")
-                .actionId("action_success"));
-        actions.add(ok);
-
-        var fail = BlockElements.button(b -> b.text(plainText("취소", true))
-                .value("fail")
-                .style("danger")
-                .actionId("action_fail"));
-        actions.add(fail);
+        actions.add(getButton("확인", "ok", "primary", "action_success"));
+        actions.add(getButton("취소", "fail", "danger", "action_fail"));
         return actions;
+    }
+
+    @NotNull
+    private BlockElement getButton(String plainText, String value, String style, String actionId) {
+        return BlockElements.button(b -> b.text(plainText(plainText, true))
+                .value(value)
+                .style(style)
+                .actionId(actionId));
     }
 }
