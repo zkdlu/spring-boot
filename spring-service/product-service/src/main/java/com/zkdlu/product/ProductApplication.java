be0d718f6ab914b9776c4efba10e3bf139ffcf1e
zkdlu <zkdlu159951@gmail.com>
2021-04-21 16:58:25 +0900

    add eureka server

--- spring-service/product-service/src/main/java/com/zkdlu/product/ProductApplication.java
+++ spring-service/product-service/src/main/java/com/zkdlu/product/ProductApplication.java
@@ -2,7 +2,9 @@ package com.zkdlu.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ProductApplication {
     public static void main(String[] args) {
