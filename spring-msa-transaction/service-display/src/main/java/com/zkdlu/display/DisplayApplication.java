10475d3e17b955f9e4ca357f6d6655f10f1eefa3
zkdlu <zkdlu159951@gmail.com>
2021-05-01 19:07:18 +0900

    display service

---
+++ spring-msa-transaction/service-display/src/main/java/com/zkdlu/display/DisplayApplication.java
@@ -0,0 +1,11 @@
+package com.zkdlu.display;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisplayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DisplayApplication.class, args);
+    }
+}
