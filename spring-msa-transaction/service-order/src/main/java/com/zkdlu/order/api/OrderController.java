10c6c9ca403d7db45ec7a747831ff18769643df4
zkdlu <zkdlu159951@gmail.com>
2021-05-11 17:03:35 +0900

    update

--- spring-msa-transaction/service-order/src/main/java/com/zkdlu/order/api/OrderController.java
+++ spring-msa-transaction/service-order/src/main/java/com/zkdlu/order/api/OrderController.java
@@ -1,5 +1,7 @@
 package com.zkdlu.order.api;
 
+import com.zkdlu.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @CrossOrigin("*")
+@RequiredArgsConstructor
 @RestController
 public class OrderController {
+
+    private final OrderService orderService;
+
     @PostMapping("/order")
     public String order(@RequestBody Cart cart) {
         log.info("hello");
