c509719384df3a21f82cca4c115fc79aca96488c
zkdlu <zkdlu159951@gmail.com>
2021-05-19 16:19:11 +0900

    thymeleaf 추가

---
+++ spring-payment/kakaopay/src/main/java/com/zkdlu/web/WebController.java
@@ -0,0 +1,21 @@
+package com.zkdlu.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class WebController {
+    @GetMapping
+    public String index() {
+        return "index.html";
+    }
+
+    @GetMapping("/order/{productId}")
+    public String order(Model model, @PathVariable String productId) {
+        model.addAttribute("productId", productId);
+
+        return "order.html";
+    }
+}
