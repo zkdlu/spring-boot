e7fb32cf6ea164c34f6759d488862f368038bcd4
zkdlu <zkdlu159951@gmail.com>
2021-06-27 20:01:41 +0900

    spring-cloud-gateway

---
+++ spring-cloud/demo/src/main/java/com/zkdlu/DemoController.java
@@ -0,0 +1,14 @@
+package com.zkdlu;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+    @GetMapping
+    public String demo() {
+        return "demo";
+    }
+}
